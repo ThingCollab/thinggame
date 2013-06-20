fb3c04f9bcbda0b74e8fcd932f2d3bb44ed31ad0
Ted <tedthesaint@gmail.com>
2013-06-19 19:37:05 -0500

    Added enemy class and player health
    Enemy can damage player. If player looses too much health, game quits.

--- enemy.py
+++ enemy.py
@@ -1,28 +1,28 @@
-import pygame, urllib, cStringIO, sys
+import pygame, urllib, cStringIO
 from pygame import *
 
 file = cStringIO.StringIO(urllib.urlopen("http://images.wikia.com/smileyofawesome/images/b/bc/Wiki.png").read())
-smiley = pygame.transform.smoothscale(pygame.image.load(file), (32,32))
+smiley = pygame.transform.smoothscale(pygame.image.load(file), (24,24))
 
-class hero(pygame.sprite.Sprite):
+class enemy(pygame.sprite.Sprite):
 	def __init__(self, startpos):
 		pygame.sprite.Sprite.__init__(self, self.groups)
-		self.health = 100
-		self.vel = [0,0]
+		self.touchDamage = 1
+		self.vel = [2,0]
 		self.image = smiley
 		self.rect = self.image.get_rect()
 		self.rect.topleft = startpos
 		self.ground = False
-	def update(self, obstacles, enemies):
+	def update(self, obstacles):
 		acc = [0,0]
 		jump = 0
-		if pygame.key.get_pressed()[K_SPACE]:
-			if self.ground: self.vel[1] -= 15
-		self.vel[0] = 0
-		if pygame.key.get_pressed()[K_RIGHT]:
-			self.vel[0] =  5
-		if pygame.key.get_pressed()[K_LEFT]:
-			self.vel[0] = -5
+#		if pygame.key.get_pressed()[K_SPACE]:
+#			if self.ground: self.vel[1] -= 15
+#		self.vel[0] = 0
+#		if pygame.key.get_pressed()[K_RIGHT]:
+#			self.vel[0] =  5
+#		if pygame.key.get_pressed()[K_LEFT]:
+#			self.vel[0] = -5
 		if not self.ground:
 			self.vel[1] += 1;
 		self.rect.left += self.vel[0]
@@ -30,18 +30,16 @@ class hero(pygame.sprite.Sprite):
 		self.rect.top += self.vel[1]
 		self.ground = False
 		self.obstacleCollide(0, self.vel[1], obstacles)
-		for enemy in enemies:
-			if sprite.collide_rect(self, enemy):
-				self.health -= enemy.touchDamage
-				print "Enemy has dealt " + str(enemy.touchDamage) + " damage to the player. New health is " + str(self.health) + "."
-		if self.health <= 0:
-			sys.exit(0)
-
+	
 	def obstacleCollide(self, velX, velY, obstacles):
 		for obstacle in obstacles:
 			if sprite.collide_rect(self, obstacle):
-				if velX > 0: self.rect.right = obstacle.rect.left
-				if velX < 0: self.rect.left = obstacle.rect.right
+				if velX > 0:
+					self.rect.right = obstacle.rect.left
+					self.vel[0] *= -1
+				if velX < 0:
+					self.rect.left = obstacle.rect.right
+					self.vel[0] *= -1
 				if velY > 0:
 					self.rect.bottom = obstacle.rect.top
 					self.ground = True
--- level.py
+++ level.py
@@ -2,18 +2,22 @@ import pygame, urllib, cStringIO
 from pygame import *
 from hero import *
 from obstacle import *
+from enemy import *
 
 allgroup = pygame.sprite.Group()
 herogroup = pygame.sprite.Group()
+enemyGroup = pygame.sprite.Group()
 obstacleGroup = pygame.sprite.Group()
 hero.groups = allgroup, herogroup
 obstacle.groups = allgroup, obstacleGroup
+enemy.groups = allgroup, enemyGroup
 
 class level:
 	
 	def __init__(self, tileDim):
 		self.levelSurface = pygame.display.set_mode((720, 480))
 		myHero = hero((64,64))
+		myEnemy = enemy((64,64))
 		tiles = []
 		levelArray = [
 		"OOOOOOOOOOOOOOOOOOOOO",
@@ -40,7 +44,8 @@ class level:
 
 	def update(self):
 		self.levelSurface.fill((63,127,255));
-		herogroup.update(obstacleGroup)
+		herogroup.update(obstacleGroup, enemyGroup)
+		enemyGroup.update(obstacleGroup)
 		obstacleGroup.update()
 		allgroup.draw(self.levelSurface)
 		pygame.display.flip()
